32dc299d2e606230e67a3bd985f0ac53550f206b
Dipesh79 <dipeshkhanal79@gmail.com>
2022-12-11 02:43:58 +0545

    Esewa Environment

--- src/LaravelEsewa.php
+++ src/LaravelEsewa.php
@@ -6,21 +6,29 @@ namespace Dipesh79\LaravelEsewa;
 class LaravelEsewa
 {
     private $merchant_id;
+    private $env;
 
     public function __construct()
     {
         $this->merchant_id = config('esewa.scd');
+        $this->env = config('esewa.env');
     }
 
-    public function esewaCheckout($amount=100,$tax_amount=0,$service_charge=0,$delivery_charge=0,$order_id,$su,$fu)
+    public function esewaCheckout($amount = 100, $tax_amount = 0, $service_charge = 0, $delivery_charge = 0, $order_id, $su, $fu)
     {
-        $esewa_url = "https://uat.esewa.com.np/epay/main/?";
+        if ($this->env == "Sandbox") {
+            $esewa_url = "https://uat.esewa.com.np/epay/main/?";
+        } elseif ($this->env == "Live") {
+            $esewa_url = "https://esewa.com.np/epay/main/?";
+        } else {
+            return "Please specify environment.";
+        }
         $data = [
             'amt' => $amount,
             'pdc' => $delivery_charge,
             'psc' => $service_charge,
             'txAmt' => $tax_amount,
-            'tAmt' => $amount+$delivery_charge+$service_charge+$tax_amount,
+            'tAmt' => $amount + $delivery_charge + $service_charge + $tax_amount,
             'pid' => $order_id,
             'scd' => $this->merchant_id,
             'su' => $su,
--- src/config/esewa.php
+++ src/config/esewa.php
@@ -2,4 +2,5 @@
 
 return array(
     'scd'=>env('ESEWA_MERCHANT_ID'),
+    'env'=>env('ESEWA_ENV'),
 );
