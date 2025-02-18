96821d6837fa9c142b49f7991ff9857254726870
Achyut Neupane <achyutkneupane@gmail.com>
2025-02-18 16:24:11 +0545

    Dynamic URL for environment

--- src/config/esewa.php
+++ src/config/esewa.php
@@ -1,6 +1,19 @@
 <?php
 
 return array(
-    'scd'=>env('ESEWA_MERCHANT_ID','EPAYTEST'),
-    'env'=>env('ESEWA_ENV','Sandbox'),
+    /**
+     * Merchant ID
+     *
+     * This is the merchant id provided by eSewa.
+     */
+    'scd' => env('ESEWA_MERCHANT_ID', 'EPAYTEST'),
+
+    /**
+     * Base URL
+     *
+     * As of Feb 18, 2025:
+     * For live environment, use https://esewa.com.np/epay/main
+     * For Sandbox environment, use https://rc.esewa.com.np/epay/main
+     */
+    'url' => env('ESEWA_URL', 'https://rc.esewa.com.np/epay/main'),
 );
